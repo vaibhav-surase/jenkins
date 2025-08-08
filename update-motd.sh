1d9c35b1a59f72a348b3749ec2bf9dc8dc229d84
Vaibhav Surase <149194370+vaibhav-surase@users.noreply.github.com>
2025-08-08 19:21:37 +0530

    Update update-motd.sh

--- update-motd.sh
+++ update-motd.sh
@@ -1,3 +1,5 @@
-#!/bin/bash
-# Script to update MOTD
-echo "hello from jenkins" | sudo tee /etc/motd > /dev/null
+/bin/bash
+echo '#!/bin/bash' | sudo tee /etc/update-motd.d/99-jenkins > /dev/null
+echo 'echo "hello from jenkins"' | sudo tee -a /etc/update-motd.d/99-jenkins > /dev/null
+sudo chmod +x /etc/update-motd.d/99-jenkins
+echo "MOTD updated successfully."
